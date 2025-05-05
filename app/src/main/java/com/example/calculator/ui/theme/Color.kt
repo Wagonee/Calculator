603b1980800f49f92f374ee650c6165b8ba193a9
Wagonee <ponomarev.ny@yandex.ru>
2025-05-05 11:47:28 +0300

    Initial commit

---
+++ app/src/main/java/com/example/calculator/ui/theme/Color.kt
@@ -0,0 +1,21 @@
+package com.example.calculator.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Universal
+val White = Color(0xFFFFFFFF)
+
+// Black theme
+val Grey = Color(0x802C2E33)
+val Green = Color(0xCC006600)
+val Blue = Color(0xCC084276)
+val Grey2 = Color(0xFF333333)
+val Grey3 = Color(0xCCFFFFFF)
+val Black = Color(0xFF000000)
+
+// Light theme
+val Grey4 = Color(0xE50D0D0D)
+val Grey5 = Color(0xFFE6E7EB)
+val Green2 = Color(0x80006600)
+val Blue2 = Color(0x80084276)
+val Grey6 = Color(0xFFD9D9D9)
